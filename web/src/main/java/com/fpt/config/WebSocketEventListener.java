c551fd0a0b42a5fd2715756027ee42b1ac147b82
quynhntv <Quynh299792>
2018-09-12 22:01:51 +0700

    sửa layout

--- web/src/main/java/com/fpt/config/WebSocketEventListener.java
+++ web/src/main/java/com/fpt/config/WebSocketEventListener.java
@@ -1,6 +1,6 @@
 package com.fpt.config;
 
-import com.fpt.entity.ChatMessage;
+//import com.fpt.entity.ChatMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
