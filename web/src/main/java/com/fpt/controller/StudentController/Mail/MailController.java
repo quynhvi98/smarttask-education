38e3492cc2bca8202c135100638a9ca42255aaf4
quynhntv <Quynh299792>
2018-09-13 11:52:18 +0700

    Thêm giao diện mail

---
+++ web/src/main/java/com/fpt/controller/StudentController/Mail/MailController.java
@@ -0,0 +1,30 @@
+package com.fpt.controller.StudentController.Mail;
+
+import com.fpt.controller.UserInfoController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Posted from Sep 13, 2018, 11:33 AM
+ *
+ * @author Vi Quynh (vi.quynh.31598@gmail.com)
+ */
+@Controller
+public class MailController {
+    private final Logger logger = LoggerFactory.getLogger(UserInfoController.class);
+
+    @RequestMapping("/mailbox")
+    public String mail(HttpServletRequest request, Model model) {
+        return "mail/mailbox";
+    }
+
+    @RequestMapping("/mailbox/compose")
+    public String compose(HttpServletRequest request, Model model) {
+        return "mail/compose";
+    }
+}
