475c69a51de5d2e010e6579fa61d6d36de3eb7e1
nam oio <oio.vivu@gmail.com>
2018-09-18 11:57:22 +0700

    Tạo đăng ký lớp học

--- persistence/src/main/java/com/fpt/repositories/lophoc/LopHocDao.java
+++ persistence/src/main/java/com/fpt/repositories/lophoc/LopHocDao.java
@@ -1,7 +1,20 @@
 package com.fpt.repositories.lophoc;
 
 import com.fpt.entity.LopHoc;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LopHocDao extends CrudRepository<LopHoc, String> {
+    @Query(value = "select * from lop_hoc ",nativeQuery = true)
+    List<LopHoc> listLopHoc();
+
+    @Query(value = "select * from lop_hoc  lh join giao_vien v on lh.giaoVien = v.ma_giao_vien join mon_hoc mh join user u on v.user_name = u.user_name where u.full_name like :gv and mh.ten_mon_hoc like :mh" ,nativeQuery = true)
+    List<LopHoc> listSearchGiaoVien(@Param("gv") String gv,@Param("mh") String mh);
+
+    @Query(value = "select * from lop_hoc lh join mon_hoc mh where lh.ma_lop like :lop and mh.ten_mon_hoc like :mh",nativeQuery = true)
+    List<LopHoc> listSearchLop(@Param("lop") String lop,@Param("mh") String mh);
+
 }
--- persistence/src/main/java/com/fpt/services/lophoc/LopHocService.java
+++ persistence/src/main/java/com/fpt/services/lophoc/LopHocService.java
@@ -1,7 +1,19 @@
 package com.fpt.services.lophoc;
 
+import com.fpt.entity.BoMon;
 import com.fpt.entity.LopHoc;
+import com.fpt.entity.MonHoc;
+import com.fpt.entity.PheDuyet;
+
+import java.util.List;
 
 public interface LopHocService {
     LopHoc taoLopHoc(LopHoc lopHoc);
+    List<LopHoc> listLopHoc();
+    List<PheDuyet> listPheDuyet();
+    PheDuyet createPheDuyet(PheDuyet pheDuyet);
+    LopHoc findById(String id);
+    List<LopHoc> searchGiaoVien(String giaovien,String bomon);
+    List<LopHoc> searchLop(String lop,String bomon);
+    List<MonHoc> listMonHoc();
 }
