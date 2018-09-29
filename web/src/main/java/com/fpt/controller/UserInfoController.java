67391d9c2c8778c89f38fa31e829d86a6d59b704
quynhntv <Quynh299792>
2018-09-29 16:34:09 +0700

    refix userinfo and lopgiangday

--- web/src/main/java/com/fpt/controller/UserInfoController.java
+++ web/src/main/java/com/fpt/controller/UserInfoController.java
@@ -17,13 +17,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.imageio.ImageIO;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
@@ -51,19 +48,19 @@ public class UserInfoController {
     public String userProfile(HttpSession session, HttpServletRequest request, Model model) {
         User userInfo = (User) session.getAttribute("userInfo");
         User user = new User();
-        if(userInfo.getUserName() != null) {
+        if (userInfo.getUserName() != null) {
             user = userService.findUserByUserName(userInfo.getUserName());
         }
 
-        if(user.getSinhVien()!= null){
+        if (user.getSinhVien() != null) {
             SinhVien sinhVien = sinhVienService.findById(userInfo.getSinhVien().getMaSinhVien());
             model.addAttribute("sinhVien", sinhVien);
             model.addAttribute("user", user);
             return "info/sinhvien";
         }
-        if(userInfo.getGiaoVien() != null){
+        if (userInfo.getGiaoVien() != null) {
             GiaoVien giaoVien = giangVienService.findById(userInfo.getGiaoVien().getMaGiaoVien());
-            model.addAttribute("giangvien",user);
+            model.addAttribute("giangvien", user);
             model.addAttribute("user", user);
             model.addAttribute("giaoVien", giaoVien);
             return "info/giangvien";
@@ -72,12 +69,12 @@ public class UserInfoController {
     }
 
     @PostMapping("/info/userUpdateImage")
-    public void userUpdateImage( HttpSession session, MultipartFile avatarFile, HttpServletRequest request) throws IOException {
+    public void userUpdateImage(HttpSession session, MultipartFile avatarFile, HttpServletRequest request) throws IOException {
         User userInfo = (User) session.getAttribute("userInfo");
         User user = userService.findUserByUserName(userInfo.getUserName());
         //TODO: fix root path by environment
         try {
-            String fileName =  UUID.randomUUID().toString().substring(0,8) + avatarFile.getOriginalFilename();
+            String fileName = UUID.randomUUID().toString().substring(0, 8) + avatarFile.getOriginalFilename();
             File newFile = new File("C:\\temp\\" + fileName);
             if (newFile.createNewFile()) {
                 FileCopyUtils.copy(avatarFile.getBytes(), newFile);
@@ -96,50 +93,44 @@ public class UserInfoController {
 //                    user.setUserAvatar(avatar);
 //                    userService.update(user);
 //                }
-            } catch (Exception e) {
-                System.out.println("Exception occured" + e.getMessage());
-            }
+        } catch (Exception e) {
+            System.out.println("Exception occured" + e.getMessage());
         }
+    }
+
     @PostMapping("/info/update")
-    public void userUpdateInfo(User user,HttpServletResponse response, HttpServletRequest request) throws IOException {
-        try {
-            if (user.getUserName() != null) {
-                User userUpdate = userService.findUserByUserName(user.getUserName());
-                if (user.getFullName() != null) {
-                    userUpdate.setFullName(user.getFullName());
-                }
-                if (user.getUserAddress() != null) {
-                    userUpdate.setUserAddress(user.getUserAddress());
-                }
-                if (user.getUserDOB() != null) {
-                    userUpdate.setUserDOB(user.getUserDOB());
-                }
-                if (user.getUserEmail() != null) {
-                    userUpdate.setUserEmail(user.getUserEmail());
-                }
-                if (user.getUserPhone() != null) {
-                    userUpdate.setUserPhone(user.getUserPhone());
-                }
-                if (user.getUserGender() != null) {
-                    userUpdate.setUserGender(user.getUserGender());
-                }
-                //Update for Giang Vien
-                if (user.getGiaoVien() != null && user.getGiaoVien().getHocHam() != null) {
-                    userUpdate.getGiaoVien().setHocHam(user.getGiaoVien().getHocHam());
-                }
-                if (user.getGiaoVien() != null && user.getGiaoVien().getMoTa() != null) {
-                    userUpdate.getGiaoVien().setMoTa(user.getGiaoVien().getMoTa());
-                }
-
-                //Update for Sinh Vien
-                //Tam thoi chua co thong tin rieng
-
-                userService.update(userUpdate);
+    public void userUpdateInfo(User user, HttpServletResponse response, HttpServletRequest request) throws IOException {
+        if (user.getUserName() != null) {
+            User userUpdate = userService.findUserByUserName(user.getUserName());
+            if (user.getFullName() != null) {
+                userUpdate.setFullName(user.getFullName());
             }
-            response.getWriter().println("success");
-        } catch (Exception e){
-            System.out.println(e.getMessage());
-            response.getWriter().println("failed");
+            if (user.getUserAddress() != null) {
+                userUpdate.setUserAddress(user.getUserAddress());
+            }
+            if (user.getUserDOB() != null) {
+                userUpdate.setUserDOB(user.getUserDOB());
+            }
+            if (user.getUserEmail() != null) {
+                userUpdate.setUserEmail(user.getUserEmail());
+            }
+            if (user.getUserPhone() != null) {
+                userUpdate.setUserPhone(user.getUserPhone());
+            }
+            if (user.getUserGender() != null) {
+                userUpdate.setUserGender(user.getUserGender());
+            }
+            //Update for Giang Vien
+            if (user.getGiaoVien() != null && user.getGiaoVien().getHocHam() != null) {
+                userUpdate.getGiaoVien().setHocHam(user.getGiaoVien().getHocHam());
+            }
+            if (user.getGiaoVien() != null && user.getGiaoVien().getMoTa() != null) {
+                userUpdate.getGiaoVien().setMoTa(user.getGiaoVien().getMoTa());
+            }
+
+            //Update for Sinh Vien
+            //Tam thoi chua co thong tin rieng
+            userService.update(userUpdate);
         }
     }
 }
